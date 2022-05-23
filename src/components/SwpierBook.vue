d2327abe7a7a15411dbc1b459a999ac92f7eaa5a
amaomaoa <347317112@qq.com>
2022-05-23 20:26:53 +0800

    c

--- src/components/SwpierBook.vue
+++ src/components/SwpierBook.vue
@@ -3,96 +3,94 @@ import type { Book } from "@/api/books";
 import { ref, computed } from "vue";
 import { imgPath } from "@/api/index";
 const props = defineProps<{
-    book: Book[];
-    msg: string;
+  book: Book[];
+  msg: string;
 }>();
 
 const bookPath = "/book/";
 const carousel = ref<HTMLFormElement>(<HTMLFormElement>{});
 
 const TwoDimensional = computed(() => {
-    let book = props.book;
-    let arr: Book[][] = new Array();
-    for (let i = 0; i < book.length; i++) {
-        let a = Math.floor(i / 6),
-            b = Math.floor(i % 6);
-        if (arr[a] == undefined) {
-            arr[a] = new Array();
-        }
-        arr[a][b] = book[i];
+  let book = props.book;
+  let arr: Book[][] = new Array();
+  for (let i = 0; i < book.length; i++) {
+    let a = Math.floor(i / 6),
+      b = Math.floor(i % 6);
+    if (arr[a] == undefined) {
+      arr[a] = new Array();
     }
-    return arr;
-});
-setTimeout(() => {
+    arr[a][b] = book[i];
+  }
+
+  setTimeout(() => {
     carousel.value.setActiveItem(0);
-}, 200);
+  }, 10);
+  return arr;
+});
 </script>
 
 <template>
-    <div class="main">
-        <div class="title">
-            <span v-for="i in msg.split('')">{{ i }}</span>
-        </div>
-        <el-carousel
-            indicator-position="none"
-            arrow="always"
-            ref="carousel"
-            :autoplay="false"
-            height="200px"
-        >
-            <el-carousel-item
-                v-for="(item, index) in TwoDimensional"
-                :key="index"
-            >
-                <ul>
-                    <li v-for="(i, j) in item">
-                        <routerLink :to="bookPath + i.id">
-                            <el-tooltip
-                                class="box-item"
-                                effect="light"
-                                :content="i.name"
-                                placement="top"
-                            >
-                                <el-image
-                                    style="width: 100px; height: 160px"
-                                    :src="imgPath + i.cover"
-                                />
-                            </el-tooltip>
-                        </routerLink>
-                    </li>
-                </ul>
-            </el-carousel-item>
-        </el-carousel>
+  <div class="main">
+    <div class="title">
+      <span v-for="i in msg.split('')">{{ i }}</span>
     </div>
+    <el-carousel
+      indicator-position="none"
+      arrow="always"
+      ref="carousel"
+      :autoplay="false"
+      height="200px"
+    >
+      <el-carousel-item v-for="(item, index) in TwoDimensional" :key="index">
+        <ul>
+          <li v-for="(i, j) in item">
+            <routerLink :to="bookPath + i.id">
+              <el-tooltip
+                class="box-item"
+                effect="light"
+                :content="i.name"
+                placement="top"
+              >
+                <el-image
+                  style="width: 100px; height: 160px"
+                  :src="imgPath + i.cover"
+                />
+              </el-tooltip>
+            </routerLink>
+          </li>
+        </ul>
+      </el-carousel-item>
+    </el-carousel>
+  </div>
 </template>
 
 <style scoped>
 li {
-    list-style: none;
-    display: inline;
-    margin: auto 30px auto 30px;
+  list-style: none;
+  display: inline;
+  margin: auto 30px auto 30px;
 }
 
 .main {
-    text-align: center;
-    width: 1200px;
-    margin: auto;
+  text-align: center;
+  width: 1200px;
+  margin: auto;
 }
 
 .title {
-    position: relative;
-    left: -500px;
-    font-size: 24px;
-    margin-top: 50px;
-    margin-bottom: 20px;
+  position: relative;
+  left: -500px;
+  font-size: 24px;
+  margin-top: 50px;
+  margin-bottom: 20px;
 }
 .title span {
-    margin: 2px;
+  margin: 2px;
 }
 
 /* 样式穿透 */
 ::v-deep(.el-carousel__arrow) {
-    color: #000000;
-    background-color: #ffffff;
+  color: #000000;
+  background-color: #ffffff;
 }
 </style>
--- src/views/BookView.vue
+++ src/views/BookView.vue
@@ -8,10 +8,10 @@ import { addtocar, type Car } from "@/api/car";
 import { ElMessage } from "element-plus";
 import router from "@/router";
 import {
-    getCommentByBookId,
-    addComment,
-    deleteComment,
-    type Comment,
+  getCommentByBookId,
+  addComment,
+  deleteComment,
+  type Comment,
 } from "@/api/comment";
 
 const route = useRoute();
@@ -22,173 +22,194 @@ const evaluation = ref<string>(""); //评论
 const userRate = ref(null);
 
 const score = computed(() => {
-    let res = 0;
-    comments.value.forEach((e) => {
-        res += e.score;
-    });
-    return res;
+  let res = 0;
+  comments.value.forEach((e) => {
+    res += e.score;
+  });
+  return res;
 });
 
 function getBookMsg() {
-    getBookById(bookid.value).then((res) => {
-        book.value = res.data.data as Book;
-        document.title = book.value.name;
-    });
+  getBookById(bookid.value).then((res) => {
+    book.value = res.data.data as Book;
+    document.title = book.value.name;
+  });
 }
 
 function getComent() {
-    getCommentByBookId(bookid.value).then((res) => {
-        comments.value = res.data.data;
-    });
+  getCommentByBookId(bookid.value).then((res) => {
+    comments.value = res.data.data;
+  });
 }
 
 function addToCaring() {
-    addtocar(<Car>{ bookId: bookid.value }).then((res) => {
-        if (res.data.code == 200) {
-            ElMessage.success("加入购物车成功");
-        } else if (res.data.code == 401) {
-            router.push("/login");
-            ElMessage.error(res.data.msg);
-        }
-    });
+  addtocar(<Car>{ bookId: bookid.value }).then((res) => {
+    if (res.data.code == 200) {
+      ElMessage.success("加入购物车成功");
+    } else if (res.data.code == 401) {
+      router.push("/login");
+      ElMessage.error(res.data.msg);
+    }
+  });
 }
 
 function init() {
-    getBookMsg();
-    getComent();
+  getBookMsg();
+  getComent();
 }
 init();
 </script>
 
 <template>
-    <div class="main">
-        <h2>{{ book.name }}</h2>
-        <br />
-        <div class="subject clearfix">
-            <div class="mainpic">
-                <img
-                    v-if="book.cover"
-                    :src="imgPath + book.cover"
-                    style="width: 150px; height: 250px"
-                />
-            </div>
-            <div class="info">
-                <ul>
-                    <li><span class="pl">作者: </span>{{ book.author }}</li>
-                    <li>
-                        <span class="pl">出版社: </span>{{ book.publisher }}
-                    </li>
-                    <li>
-                        <span class="pl">出版时间: </span>{{ book.publishDate }}
-                    </li>
-                    <li><span class="pl">价格: </span>{{ book.price }}</li>
-                    <li><span class="pl">库存: </span>{{ book.stock }}</li>
-                    <li>
-                        <div class="addcar" @click="addToCaring">
-                            加入购物车
-                        </div>
-                    </li>
-                </ul>
-            </div>
+  <div class="main">
+    <h2>{{ book.name }}</h2>
+    <br />
+    <div class="subject clearfix">
+      <div class="mainpic">
+        <img
+          v-if="book.cover"
+          :src="imgPath + book.cover"
+          style="width: 150px; height: 250px"
+        />
+      </div>
+      <div class="info">
+        <ul>
+          <li><span class="pl">作者: </span>{{ book.author }}</li>
+          <li><span class="pl">出版社: </span>{{ book.publisher }}</li>
+          <li><span class="pl">出版时间: </span>{{ book.publishDate }}</li>
+          <li><span class="pl">价格: </span>{{ book.price }}</li>
+          <li><span class="pl">库存: </span>{{ book.stock }}</li>
+          <li>
+            <div class="addcar" @click="addToCaring">加入购物车</div>
+          </li>
+        </ul>
+      </div>
+
+      <div class="myscore">
+        <div style="height: 200px; display: inline-block">
+          <p style="color: #9b9b9b9b">读者评分</p>
+          <span class="ll rating_num">{{ score }}</span>
         </div>
-        <div class="related_info">
-            <h3 style="color: #007722">内容简介 · · · · · ·</h3>
-            <p class="details" v-for="line in book.details?.split('\n')">
-                {{ line }}
-            </p>
-        </div>
-        <div class="evaluation">
-            <h3 style="color: #007722">读者评价 · · · · · ·</h3>
-            <div class="details">
-                <el-input
-                    v-model="evaluation"
-                    :autosize="{ minRows: 2, maxRows: 4 }"
-                    type="textarea"
-                    placeholder="请输入评价"
-                />
-                <div style="margin-top: 5px">
-                    <el-rate v-model="userRate" style="display: inline" />
-                    <el-button style="float: right">确定</el-button>
-                </div>
-            </div>
-        </div>
+      </div>
     </div>
+    <div class="related_info">
+      <h3 style="color: #007722">内容简介 · · · · · ·</h3>
+      <p class="details" v-for="line in book.details?.split('\n')">
+        {{ line }}
+      </p>
+    </div>
+    <div class="evaluation">
+      <h3 style="color: #007722">读者评价 · · · · · ·</h3>
+      <div class="details">
+        <el-input
+          v-model="evaluation"
+          :autosize="{ minRows: 2, maxRows: 4 }"
+          type="textarea"
+          placeholder="请输入评价"
+        />
+        <div style="margin-top: 5px">
+          <el-rate v-model="userRate" style="display: inline" />
+          <el-button style="float: right">确定</el-button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
 .main {
-    width: 1000px;
-    margin: auto;
-    margin-top: 100px;
+  width: 1000px;
+  margin: auto;
+  margin-top: 100px;
 }
 
 .subject {
-    position: relative;
-    left: 20px;
-    width: 500px;
+  position: relative;
+  left: 20px;
+  width: 500px;
 }
 .subject::after {
-    content: "";
-    display: block;
-    clear: both;
+  content: "";
+  display: block;
+  clear: both;
 }
 .clearfix {
-    display: block;
+  display: block;
 }
 
 .mainpic {
-    margin: 3px 0 0 0;
-    float: left;
-    text-align: center;
-    margin: 3px 12px 0 0;
-    max-width: 155px;
-    overflow: hidden;
+  margin: 3px 0 0 0;
+  float: left;
+  text-align: center;
+  margin: 3px 12px 0 0;
+  max-width: 155px;
+  overflow: hidden;
 }
 #info {
-    max-width: 600px;
+  max-width: 600px;
 }
 
 li {
-    list-style: none;
-    margin-bottom: 6px;
+  list-style: none;
+  margin-bottom: 6px;
 }
 .pl {
-    font: 16px Arial, Helvetica, sans-serif;
-    line-height: 150%;
-    color: #666666;
+  font: 16px Arial, Helvetica, sans-serif;
+  line-height: 150%;
+  color: #666666;
 }
 
 .info {
-    float: left;
-    max-width: 248px;
-    word-wrap: break-word;
+  float: left;
+  max-width: 248px;
+  word-wrap: break-word;
 }
 
 .related_info {
-    margin-top: 50px;
-    position: relative;
-    left: 20px;
+  margin-top: 50px;
+  position: relative;
+  left: 20px;
 }
 
 .details {
-    position: relative;
-    left: 20px;
+  position: relative;
+  left: 20px;
 }
 .evaluation {
-    position: relative;
-    left: 20px;
-    margin-top: 50px;
-    margin-bottom: 100px;
+  position: relative;
+  left: 20px;
+  margin-top: 50px;
+  margin-bottom: 100px;
 }
 
 .addcar {
-    margin-top: 20px;
-    text-align: center;
-    cursor: pointer;
-    width: 120px;
-    border-radius: 15px;
-    border: 10px;
-    color: aliceblue;
-    background-color: cadetblue;
+  margin-top: 20px;
+  text-align: center;
+  cursor: pointer;
+  width: 120px;
+  border-radius: 15px;
+  border: 10px;
+  color: aliceblue;
+  background-color: cadetblue;
+}
+.myscore {
+  position: relative;
+  left: 300px;
+  width: 100px;
+  height: 80px;
+  display: inline-block;
+}
+
+.el-divider--vertical {
+  display: inline-block;
+  width: 1px;
+  height: 200px;
+  margin: 0 8px;
+  vertical-align: middle;
+  position: relative;
+}
+
+.rating_num {
+  font-size: 30px;
 }
 </style>
