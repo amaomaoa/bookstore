5f44b89e5740f30e31ea0fa6691cc9e3a0444ad1
amaomaoa <347317112@qq.com>
2022-06-07 14:34:23 +0800

    vue

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import "element-plus/dist/index.css";
 import { createPinia } from 'pinia'
+import 'element-plus/theme-chalk/dark/css-vars.css'
 
 const app = createApp(App)
 
